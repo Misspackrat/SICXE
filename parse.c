29d3b02aa2afd96d1acb2521e978a9e46abbd026
Misspackrat <56569688+Misspackrat@users.noreply.github.com>
2019-11-08 11:21:50 -0800

    Create parse.c

---
+++ parse.c
@@ -0,0 +1,12 @@
+//parse function, takes in word by word. Returns length and updates pointer given to it
+#include <stdbool.h> 
+#include <stdio.h> 
+#include <string.h> 
+#include <stdlib.h> 
+
+int parse(char *w){
+    int count = 0;
+
+
+    return count;
+}
