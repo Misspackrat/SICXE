cf6286ae1a89f74e4944f289d0fa312b0a1ca767
Misspackrat <56569688+Misspackrat@users.noreply.github.com>
2019-11-08 12:39:22 -0800

    Update main.c

--- main.c
+++ main.c
@@ -9,7 +9,12 @@
 #include <sys/wait.h>
 #include <signal.h>
 int main(int argc, char *argv[])
-{
+{ 
+  if((input_fd = open(argv[1],O_RDONLY))<0){ //if file does not open, send an error and exit
+    perror("oops, no file opened");
+    exit(1);
+  }
+  dup2(input_fd,STDIN_FILENO); //take input from the text file
   pass1();
   pass2();
   exit(0); 
