c15f56ea2e8148ace45f5f2b37c65cfbb5e8e07a
Misspackrat <56569688+Misspackrat@users.noreply.github.com>
2019-11-08 12:18:51 -0800

    Update main.c

--- main.c
+++ main.c
@@ -1,1 +1,16 @@
 //write out some stuff
+#include <fcntl.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <sys/wait.h>
+#include <signal.h>
+int main(int argc, char *argv[])
+{
+  pass1();
+  pass2();
+  exit(0); 
+}
