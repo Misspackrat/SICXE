e390f910a5eb8422f3feed8407290ba469bb1c0f
Lauryn Jefferson <lajeff77@gmail.com>
2019-11-29 17:25:06 -0800

    reading file

--- pass2.c
+++ pass2.c
@@ -1,5 +1,45 @@
 #include <stdio.h>
 
+//forward declarations
+
+void readData();
+struct opcode;
+
+int main()
+{
+    readData();
+}
+
+//read data method, used to read opcode.txt and make an array of opcodes
+void readData()
+{
+    //open the file
+    FILE* fptr;
+    fptr = fopen("opcode.txt","rb");
+
+    //testing to print something out
+    char ch = fgetc(fptr);
+    while ((ch != '\n') && (ch != EOF)) 
+    {
+        printf("%c",ch);
+        ch = fgetc(fptr);
+    }
+    //close the file
+    fclose(fptr);
+
+    //function psuedo code
+    //while there are more lines in the file
+        //read line of file
+        //make a new struct
+        //insert the mnuemonic
+        //insert the opcode
+        //insert the format
+        //add the struct to an array
+        //move to the next line
+    //done reading data
+}
+
+
 //opcode.txt is formatted as mnuemonic, opcode, format
 struct opcode
 {
@@ -7,3 +47,5 @@ struct opcode
     int opcode;
     int format;
 };
+
+
