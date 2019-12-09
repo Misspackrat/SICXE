fc5d581fe436c45bfeaa615f3c7e73654f2a322c
SiennaRose <44793381+SiennaRose@users.noreply.github.com>
2019-12-08 19:55:32 -0800

    labels are included in the symtab

--- main.c
+++ main.c
@@ -6,16 +6,23 @@
 #include <unistd.h>
 #include <sys/types.h>
 #include <sys/stat.h>
-#include <sys/wait.h>
+//#include <sys/wait.h>
 #include <signal.h>
+
+#include "parse.h"
+#include "pass1.c"
+
 int main(int argc, char *argv[])
 { 
+    int input_fd; 
+
   if((input_fd = open(argv[1],O_RDONLY))<0){ //if file does not open, send an error and exit
     perror("oops, no file opened");
     exit(1);
   }
   dup2(input_fd,STDIN_FILENO); //take input from the text file
+
   pass1();
-  pass2();
+  //pass2();
   exit(0); 
 }
--- pass1.c
+++ pass1.c
@@ -5,46 +5,121 @@
 //13 Decemeber 2019
 //metacharacters: +, #, @ note these
 
-struct line
+#define MAX 10
+
+struct Line
 {
-	char[6] label;
-	char[6] opcode;
-	char[20] operand;
+	char label[MAX];
+	char opcode[MAX];
+	char operand[MAX];
+	char metaChar[MAX];
 };
 
 void pass1()
 {
-	int e = -1; //e will be 0 if parse returns a new line, -1 when EOF is found, or the length of the string if found 
+	struct Line line; 
+	int length = 0; //e will be 0 if parse returns a new line, -1 when EOF is found, or the length of the string if found 
 	int locctr = 0; //location counter
 	int progLength = 0;
-	char d[1000];  //update this length to something appropriate
-
-	//create new intermediate file
-	//create new symtab file
+	char buffer[MAX];  //update this length to something appropriate
 
 	//open both files for writing in
+	//create new intermediate file
+	FILE* interFile = fopen("intermediate.txt", "w");
 
-		//begin
-		  //read first input line
-	do //loops through all the lines of the SICXE program until EOF
+	//create new symtab file
+	FILE* symFile = fopen("symtab.txt", "w");
+
+	(line.label)[0] = '\0';
+	(line.opcode)[0] = '\0';
+	(line.operand)[0] = '\0';
+	(line.metaChar)[0] = '\0';
+
+	while (length != -1)
 	{
-		do //loops through all elements in one line of SICXE code
+		length = parse(buffer);
+		printf("%s is length %d\n", buffer, length);
+		
+		//////////////create struct to hold label, opcode, and operand//////////////////
+		//search through opcode.txt to see if string matches any opcodes or if it is a label
+		char str[10];
+		bool op = false; 
+		FILE* optxtFile = fopen("opcodeTEXT.txt", "r");
+		if (optxtFile == NULL)
 		{
-			e = parse(d);  //every call to parse will return one word or one metacharacter
+			printf("Could not open opcodeTEXT.txt");
+			exit(1);
+		}
 
-			//////////////create struct to hold label, opcode, and operand//////////////////
-			//search through opcode.txt to see if string matches any opcodes or if it is a label
-			//if string does match an opcode
-				//set char[] label in struct to null
-			//else
-				//set char[] label to string
-			//set opcode
-			//set operand
-			////////////////////////////////////////////////////////////////////////////////
+		while (fgets(str, 10, optxtFile) != NULL) 
+		{
+			//get rid of newline character at end of str
+			int len = strlen(str);
+			str[len - 1] = 0; 
 
-		} while (e > 0);  //e will be 0 if parse returns a new line or -1 when EOF is found
+			int val = strcmp(buffer, str);
+			//printf("buffer: %s, str: %s, val = %d\n", buffer, str,val);
+			if (val == 0)
+			{
+				op = true;
+				//printf("THEY ARE THE SAME\n");
+			}
+				 
+		
+		}
+		fclose(optxtFile);
+		
+		//if buffer does match an opcode, set opcode to buffer
+		//otherwise set label to buffer
+		if (op)
+		{
+			strcpy(line.opcode, buffer);
+			//printf("line.opcode %s\n", line.opcode);
+		}
+		else
+		{
+			//check if buffer is a meta character
+			bool meta = false; 
+			if (strcmp(buffer, "#") == 0)
+			{
+				meta = true;
+				strcpy(line.metaChar, buffer);
+			}
+			else
+				if (strcmp(buffer, "+") == 0)
+				{
+					meta = true;
+					strcpy(line.metaChar, buffer);
+				}
+				else
+					if (strcmp(buffer, "@") == 0)
+					{
+						meta = true;
+						strcpy(line.metaChar, buffer);
+					}
 
-		  //if 'START' is in opcode --> get from struct
+			//check if buffer is an operand
+			bool operand = false; 
+			printf("%s line.opcode\n\n", line.opcode);
+			if (!meta && ((line.opcode)[0] != '\0'))
+			{
+				operand = true; 
+
+			}
+
+			//save buffer into label if it is neither meta character or operand
+			if (!meta && !operand) 
+			{
+				strcpy(line.label, buffer);
+				strcat(line.label, "\n");
+				fputs(line.label, symFile);
+			}
+		}
+			 
+		//set operand
+		////////////////////////////////////////////////////////////////////////////////
+
+		//if 'START' is in opcode --> get from struct
 			  //save #operand at starting address (ex: START 1000)
 			  //initialize LOCCTR to starting address
 		  //else
@@ -59,12 +134,26 @@ void pass1()
 		  //update LOCCTR
 
 		  //write line to intermediate file = LOCCTR + (optional)LABEL + OPCODE + OPERAND --> get data from struct 
+
+		//reset line struct to empty all current data
+		if (length == 0)
+		{
+			(line.label)[0] = '\0';
+			(line.opcode)[0] = '\0';
+			(line.operand)[0] = '\0';
+			(line.metaChar)[0] = '\0';
+		}
+		
+
 		  //read next input line --> (already done with do-while loop)
-	} while (e != -1);
+
+	}
 
 	//after writing the last line to the intermediate file 
 	//save (LOCCTR - starting address) as program length	 
 
 	//close files we opened
+	fclose(interFile);
+	fclose(symFile);
 
 }
